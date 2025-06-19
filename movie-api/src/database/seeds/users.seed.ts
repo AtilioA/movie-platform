f9965b06c1f1a0cfb5e3d6de3ef82cb499c879eb
AtilioA <27153776+AtilioA@users.noreply.github.com>
2025-06-19 19:23:06 -0300

    chore: add more seed users

--- movie-api/src/database/seeds/users.seed.ts
+++ movie-api/src/database/seeds/users.seed.ts
@@ -1,4 +1,12 @@
 export const userSeeds = [
-  { id: '12e1e80a-f58a-42f1-a195-56780f40cc75', email: 'user1@example.com', name: 'User One', password: 'password123' },
-  { id: 'b39aec49-8821-447f-a843-b1fd44dc31a2', email: 'user2@example.com', name: 'User Two', password: 'password123' },
+  { id: '12e1e80a-f58a-42f1-a195-56780f40cc75', email: 'seed_user1@example.com', name: 'Seed User One', password: 'Password123!' },
+  { id: 'b39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user2@example.com', name: 'Seed User Two', password: 'Password123!' },
+  { id: 'c39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user3@example.com', name: 'Seed User Three', password: 'Password123!' },
+  { id: 'd39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user4@example.com', name: 'Seed User Four', password: 'Password123!' },
+  { id: 'e39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user5@example.com', name: 'Seed User Five', password: 'Password123!' },
+  { id: 'f39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user6@example.com', name: 'Seed User Six', password: 'Password123!' },
+  { id: 'g39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user7@example.com', name: 'Seed User Seven', password: 'Password123!' },
+  { id: 'h39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user8@example.com', name: 'Seed User Eight', password: 'Password123!' },
+  { id: 'i39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user9@example.com', name: 'Seed User Nine', password: 'Password123!' },
+  { id: 'j39aec49-8821-447f-a843-b1fd44dc31a2', email: 'seed_user10@example.com', name: 'Seed User Ten', password: 'Password123!' },
 ];
