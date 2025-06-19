4575ee7ff97dbc889e71746f0164bc10d467df42
AtilioA <27153776+AtilioA@users.noreply.github.com>
2025-06-19 20:08:45 -0300

    fix: import movies module from ratings

--- movie-api/src/ratings/ratings.module.ts
+++ movie-api/src/ratings/ratings.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RatingsController } from './ratings.controller';
 import { RatingsService } from './ratings.service';
 import { Rating } from './entities/rating.entity';
+import { MoviesModule } from '../movies/movies.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rating])],
+  imports: [
+    TypeOrmModule.forFeature([Rating]),
+    MoviesModule
+  ],
   controllers: [RatingsController],
   providers: [RatingsService],
   exports: [RatingsService, TypeOrmModule],
