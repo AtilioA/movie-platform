94f9268163b3879ebefc7efda4fb2da179f21a92
AtilioA <27153776+AtilioA@users.noreply.github.com>
2025-06-18 21:16:52 -0300

    feat: add modules for movies, actors, ratings

--- movie-api/src/app.module.ts
+++ movie-api/src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { MoviesModule } from './movies/movies.module';
+import { ActorsModule } from './actors/actors.module';
+import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
   imports: [
@@ -11,6 +14,9 @@ import { DatabaseModule } from './database/database.module';
       envFilePath: '.env',
     }),
     DatabaseModule,
+    MoviesModule,
+    ActorsModule,
+    RatingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
