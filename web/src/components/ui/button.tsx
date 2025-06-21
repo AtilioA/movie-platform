dbadf291ca5795c360a785815147030a602a18ba
Atílio <27153776+AtilioA@users.noreply.github.com>
2025-06-20 23:09:35 -0300

    feat: create search-input

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -18,7 +18,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 hover:underline",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
@@ -43,7 +43,7 @@ interface ButtonProps extends React.ComponentProps<"button">, VariantProps<typeo
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, children, href, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    
+
     const button = (
       <Comp
         data-slot="button"
